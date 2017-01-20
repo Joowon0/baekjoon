6d63544b716682b6684166eef255162d42dba009
Joowon <joowon7508@gmail.com>
2017-01-20 21:04:39 +0900

    trying 03163

--- 03163fallingAnt.cpp
+++ 03163fallingAnt.cpp
@@ -25,6 +25,7 @@ int main() {
 
     // calculate
     for (int sec = 1; true; sec++) {
+      /*
       if (antTemp[0][1] !=  -1) {
 	// if collision
 	if (antTemp[0][1] == antTemp[1][1])
@@ -34,13 +35,19 @@ int main() {
 	  antTemp2[0][0] = antTemp[0][0] + 1;
 	else
 	  antTemp2[i][0] = antTemp[i][0] - 1;
+      */
       }
       // for every [i] and [i+1]
-      for (int i = 1; i < antNum - 1; i++) {
+      for (int i = 0; i < antNum; i++) {
 	if (antTemp[i][1] != -1) { // skip fallen ants
 	  // if collision
-	  if (antTemp[i][1] == antTemp[i+1][1])
+	  if (i != 0 && antTemp[i][1] == antTemp[i+1][1])
 	    antTemp[i][0] *= -1;
+	  else if (i != antNum && antTemp[i][1] == antTemp[i+1][1])
+	    antTemp[i][0] *= -1;
+	  
+	  //if (antTemp[i][1] == antTemp[i+1][1] || antTemp[i][1] == antTemp[i-1][1])
+	  //antTemp[i][0] *= -1;
 	// go 1mm
 	if (antTemp[i][0] == 1)
 	  antTemp2[i][0] = antTemp[i][0] + 1;
@@ -50,8 +57,18 @@ int main() {
       }
 
       // check if fallen
+      for (int i = 0; i < antNum; i++)
+	if (antTemp2[i][1] < 0 || antTemp2[i][1] > stickLen)
+	  antTemp2[i][0] = -1;
 
       // copy antTemp2 to antTemp
+      for (int i = 0; i < antNum; i++) {
+	antTemp[i][0] = antTemp2[i][0];
+	antTemp[i][1] = antTemp2[i][1];
+
+	cout << antTemp[i][1] << " " ;
+      }
+      cout << endl;
     }
   }
 
