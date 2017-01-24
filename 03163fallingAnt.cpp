45c3a57424bd9c4935d42273a224c7c93ae7b696
Joowon <joowon7508@gmail.com>
2017-01-24 16:36:53 +0900

    solved but wrong answer

--- 03163fallingAnt.cpp
+++ 03163fallingAnt.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <stdlib.h> // qsort
 
 using namespace std;
+
+int compare (const void * a, const void * b) {
+  return ( (*(int*) a) - (*(int*) b) );
+}
 
 int main() {
   int caseNum;
@@ -8,12 +13,19 @@ int main() {
   int antIn[100000][2]; // [n][0] : ID
                         // [n][1] : position
   int antTemp[100000][2], antTemp2[100000][2];
+  int inIdOrder[10000][2]; // [n][0] : abs (ID)
+                           // [n][1] : index
 
+  int cnt;
+  bool ifEnd = 0;
+  
   cin >> caseNum;
 
   while(caseNum--) {
     // get input
     cin >> antNum >> stickLen >> nth;
+
+    cnt = 0;
 
     for (int i = 0; i < antNum; i++) {
       cin >> antIn[i][1] >> antIn[i][0];
@@ -21,52 +33,99 @@ int main() {
       if (antIn[i][0] < 0)
 	antTemp[i][0] = -1;
       antTemp[i][1] = antIn[i][1];
+
+      if (antIn[i][0] > 0)
+	inIdOrder[i][0] = antIn[i][0] - 1;
+      else
+	inIdOrder[i][0] = (antIn[i][0] + 1);
+      inIdOrder[i][1] = i;
     }
+
+    // fill in inIdOrder[][]
+    qsort (inIdOrder, antNum, sizeof(int) * 2, compare);
+
+    for (int i = 0; i < antNum; i++)
+      if (inIdOrder[i][0] < 0)
+	inIdOrder[i][0] *= -1;
+
+    /*
+    // print for test
+    for (int i = 0; i < antNum; i++) {
+      cout << inIdOrder[i][0] << " " << inIdOrder[i][1] << endl;
+    }
+    
+    for (int i = 0; i < antNum; i++) {
+      cout << antTemp[i][1] << " " << antTemp[i][0] << "\t";
+    }
+    cout << endl;
+    */
 
     // calculate
     for (int sec = 1; true; sec++) {
-      /*
-      if (antTemp[0][1] !=  -1) {
-	// if collision
-	if (antTemp[0][1] == antTemp[1][1])
-	  antTemp[0][0] *= -1;
-	// go 1mm
-	if (antTemp[0][0] == 1)
-	  antTemp2[0][0] = antTemp[0][0] + 1;
-	else
-	  antTemp2[i][0] = antTemp[i][0] - 1;
-      */
-      }
       // for every [i] and [i+1]
       for (int i = 0; i < antNum; i++) {
-	if (antTemp[i][1] != -1) { // skip fallen ants
+	if (antTemp[i][0] != 0) { // skip fallen ants
 	  // if collision
-	  if (i != 0 && antTemp[i][1] == antTemp[i+1][1])
+	  // case1 : when at the same place
+	  if (i != antNum -1 && antTemp[i+1][1] == antTemp[i][1]) {
 	    antTemp[i][0] *= -1;
-	  else if (i != antNum && antTemp[i][1] == antTemp[i+1][1])
+	    antTemp[i+1][0] *= -1;
+	  }
+	  /*
+	  else if (i != antNum && antTemp[i][1] == antTemp[i-1][1]) {
 	    antTemp[i][0] *= -1;
+	    antTemp[i-1][0] *= -1;
+	  }
+	  */
+
+	  // case2 : when about to collid in 0.5 sec
+	  else if (i != 0 && antTemp[i+1][1] - antTemp[i][1] == 1 && antTemp[i+1][0] == -1 && antTemp[i][0] == 1) {
+	    //cout << "collid : " << i << " " << i+1 << endl;
+	    antTemp[i][0] *= -1;
+	    antTemp[i+1][0] *= -1;
+	    i ++;
+	    break;
+	  }
 	  
-	  //if (antTemp[i][1] == antTemp[i+1][1] || antTemp[i][1] == antTemp[i-1][1])
-	  //antTemp[i][0] *= -1;
+	  
+	  
 	// go 1mm
 	if (antTemp[i][0] == 1)
-	  antTemp2[i][0] = antTemp[i][0] + 1;
+	  antTemp2[i][1] = antTemp[i][1] + 1;
 	else
-	  antTemp2[i][0] = antTemp[i][0] - 1;
+	  antTemp2[i][1] = antTemp[i][1] - 1;
 	}
       }
 
+      ifEnd = 0;
+      
       // check if fallen
-      for (int i = 0; i < antNum; i++)
-	if (antTemp2[i][1] < 0 || antTemp2[i][1] > stickLen)
-	  antTemp2[i][0] = -1;
+      for (int i = 0; i < antNum; i++) {
+	if (antTemp[inIdOrder[i][1]][0] != 0 && (antTemp2[inIdOrder[i][1]][1] < 0 || antTemp2[inIdOrder[i][1]][1] > stickLen)) {
+	  antTemp[inIdOrder[i][1]][0] = 0;
+
+	  // check if ends
+	  cnt++;
+
+	  cout << cnt << " " << inIdOrder[i][1] << " " << antIn[inIdOrder[i][1]][0] << endl;
+	  if (cnt == nth) {
+	    ifEnd = 1;
+	    cout << antIn[inIdOrder[i][1]][0] << endl;
+	    break;
+	  }
+	}
+      }
+
+      if (ifEnd == 1)
+        break;
+	  
 
       // copy antTemp2 to antTemp
       for (int i = 0; i < antNum; i++) {
-	antTemp[i][0] = antTemp2[i][0];
+	//antTemp[i][0] = antTemp2[i][0];
 	antTemp[i][1] = antTemp2[i][1];
 
-	cout << antTemp[i][1] << " " ;
+	cout << antTemp[i][1] << " " << antTemp[i][0] << "\t";
       }
       cout << endl;
     }
